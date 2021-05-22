9ba5294d9112321263b6b1fb6ee6cff8e0736b52
SantiagoCirco <Santiago.circo.e@gmail.com>
2021-05-22 14:57:42 -0300

    Added imagen's import to paquete's model

--- models/Paquete.js
+++ models/Paquete.js
@@ -2,6 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../db/index");
 const Compra = require("./Compra");
 const Fecha  = require('./Fecha');
+const Imagen = require('./Imagen');
 
 const Paquete = sequelize.define(
 	"paquete",
